1b314e8640f31b3ef6a1d824f430df0194fbcc19
Lab2-11 <Lab2-11@LAB2-11>
2025-03-11 17:53:06 -0600

    primera parte

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace _2doParcial
 {
     internal class Program
     {
+        //Sergio Reyes Alfaro
+
+        public enum Menu
+        {
+            AgregarTel = 1, MostrarTel, ActualizarTel, EliminarTel, Salir
+        }
         static void Main(string[] args)
         {
+            
+        }
+        static Menu menu()
+        {
+            Console.WriteLine("Escoge la opción que quieras hacer:");
+            Console.WriteLine("1) Agregar teléfono");
+            Console.WriteLine("2) Mostrar teléfono");
+            Console.WriteLine("3) Actualizar teléfono");
+            Console.WriteLine("4) Eliminar teléfono");
+            Console.WriteLine("5) Salir");
+
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+            return opc;
         }
     }
 }
---
+++ Telefonos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2doParcial
+{
+    internal class Telefonos
+    {
+        public Telefonos(int almacenamiento, int año, double precio, string color, int numeroCamaras)
+        {
+            Almacenamiento = almacenamiento;
+            Año = año;
+            Precio = precio;
+            Color = color;
+            NumeroCamaras = numeroCamaras;
+        }
+        public Telefonos() { }
+
+        public int Almacenamiento { get; set; }
+        public int Año { get; set; }
+        public double Precio { get; set; }
+        public string Color { get; set; }
+        public int NumeroCamaras { get; set; }
+    }
+}
